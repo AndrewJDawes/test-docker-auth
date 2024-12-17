6725434c5e3a340c26e9ed171c9b122d68e8df06
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-12-17 02:42:51 +0000

    Remove references to arch

--- docker-auth-config/test.sh
+++ docker-auth-config/test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-ARCH="${ARCH:-amd64}"
 DOCKER_SOCK="${DOCKER_SOCK:-/var/run/docker.sock}"
 DOCKER_CONFIG_DIR="${DOCKER_CONFIG_DIR:-$HOME/.docker/}"
 mkdir -p "$DOCKER_CONFIG_DIR"
-docker build --build-arg ARCH=${ARCH} -t docker-auth-config:test .
+docker build -t docker-auth-config:test .
 docker volume ls | grep "gh_cli_docker_volume_v1" || docker volume create "gh_cli_docker_volume_v1"
 docker run --rm -v gh_cli_docker_volume_v1:/root/.config/gh -v "$DOCKER_SOCK:/var/run/docker.sock" -v "$DOCKER_CONFIG_DIR:/root/.docker/" --env HOST_UID=${UID} --env HOST_GID=${GID} docker-auth-config:test
